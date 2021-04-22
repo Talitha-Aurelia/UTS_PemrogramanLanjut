5038e63cb8010c442b02b6a49e5e21e776ac4a0a
TalithaAurelia <80021785+Talitha-Aurelia@users.noreply.github.com>
2021-04-22 09:21:03 +0700

    Add files via upload

---
+++ UTS_NOMOR2/src/UTS/PemrogamanLanjut/Person.java
@@ -0,0 +1,18 @@
+// Nama : Talitha Aurelia Shakira
+// NIM  : 11200930000023
+// Kelas : 2A
+
+package UTS.PemrogamanLanjut;
+public class Person {
+    public String name;
+    public int age;
+    public void Person1(String name, int age){
+       System.out.println("Halo My name is:"+ name);
+       System.out.println("Halo My age is:" + age);
+   }
+    public void Person2(String name, int age){
+       System.out.println("Halo My name is:"+ name);
+       System.out.println("Halo My age is:" + age);
+   } 
+     
+}
---
+++ UTS_NOMOR2/src/UTS/PemrogamanLanjut/PersonBeraksi.java
@@ -0,0 +1,17 @@
+// Nama : Talitha Aurelia Shakira
+// NIM  : 11200930000023
+// Kelas : 2A
+
+package UTS.PemrogamanLanjut;
+public class PersonBeraksi {
+   public static void main (String[]args){  
+        
+    {
+       Person object = new Person ();
+        object.Person1("Joe Smith", 90);
+        object.Person2("Mary Sharp", 80);
+        
+    }
+   
+}
+}
