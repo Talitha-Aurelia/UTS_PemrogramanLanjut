62ba3c277b29297352654f71a3ad170a50c27365
TalithaAurelia <80021785+Talitha-Aurelia@users.noreply.github.com>
2021-04-22 09:22:17 +0700

    Add files via upload

---
+++ UTS_NOMOR4/src/UTS/PemrogamanLanjut/Mahasiswa.java
@@ -0,0 +1,10 @@
+// Nama : Tallitha Aurelia Shakira
+// NIM  : 11200930000023
+// Kelas : 2A
+
+package UTS.PemrogamanLanjut;
+public class Mahasiswa {
+ String Belajar;
+ String Berenang;
+ String Beryanyi;
+}
---
+++ UTS_NOMOR4/src/UTS/PemrogamanLanjut/MahasiswaBeraksi.java
@@ -0,0 +1,19 @@
+// Nama : Talitha Aurelia Shakira
+// NIM  : 11200930000023
+// Kelas : 2A
+
+package UTS.PemrogamanLanjut;
+public class MahasiswaBeraksi {
+public static void main(String[] args){
+        Mahasiswa Aurel = new Mahasiswa();
+        Aurel.Belajar= "Aurel sangat rajin belajar";
+        Aurel.Berenang= "Aurel senang sekali belajar berenang, agar bisa menggapain cita-citanya menjadii seorang atletis";
+        Aurel.Beryanyi= "Selain berenang, Aurel juga gemar bernyanyi";
+        
+        System.out.println(Aurel.Belajar);
+        System.out.println(Aurel.Berenang);
+        System.out.println(Aurel.Beryanyi);
+  }     
+       
+
+}
